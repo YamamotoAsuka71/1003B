464e4b063de45405270d3457662dd8a180b917eb
yutyann5213 <yutyanntyenn5213@gmail.com>
2023-10-24 16:01:01 +0900

    update エネミーの視点の追尾

--- Action3D/Assets/Scripts/Enemy02Controller.cs
+++ Action3D/Assets/Scripts/Enemy02Controller.cs
@@ -7,14 +7,13 @@ public class Enemy02Controller : MonoBehaviour
     [SerializeField] private GameObject Enemy02;
     private float interval;
     private float time = 0f;
-    // ターゲットオブジェクトの Transformコンポーネントを格納する変数
-    public Transform target;
-    // オブジェクトの移動速度を格納する変数
-    public float moveSpeed;
-    // オブジェクトが停止するターゲットオブジェクトとの距離を格納する変数
-    public float stopDistance;
-    // オブジェクトがターゲットに向かって移動を開始する距離を格納する変数
-    public float moveDistance;
+    // 自身のTransform
+    [SerializeField] private Transform _self;
+    // ターゲットのTransform
+    [SerializeField] private Transform _target;
+    // 前方の基準となるローカル空間ベクトル
+    [SerializeField] private Vector3 _forward = Vector3.forward;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,25 +22,16 @@ public class Enemy02Controller : MonoBehaviour
 
     private void Update()
     {
-        //CreateEnemy();
-        // 変数 targetPos を作成してターゲットオブジェクトの座標を格納
-        Vector3 targetPos = target.position;
-        // 自分自身のY座標を変数 target のY座標に格納
-        //（ターゲットオブジェクトのX、Z座標のみ参照）
-        targetPos.y = transform.position.y;
-        // オブジェクトを変数 targetPos の座標方向に向かせる
-        transform.LookAt(targetPos);
+        // ターゲットへの向きベクトル計算
+        var dir = _target.position - _self.position;
 
-        // 変数 distance を作成してオブジェクトの位置とターゲットオブジェクトの距離を格納
-        float distance = Vector3.Distance(transform.position, target.position);
-        // オブジェクトとターゲットオブジェクトの距離判定
-        // 変数 distance（ターゲットオブジェクトとオブジェクトの距離）が変数 moveDistance の値より小さければ
-        // さらに変数 distance が変数 stopDistance の値よりも大きい場合
-        if (distance < moveDistance && distance > stopDistance)
-        {
-            // 変数 moveSpeed を乗算した速度でオブジェクトを前方向に移動する
-            transform.position = transform.position + transform.forward * moveSpeed * Time.deltaTime;
-        }
+        // ターゲットの方向への回転
+        var lookAtRotation = Quaternion.LookRotation(dir, Vector3.up);
+        // 回転補正
+        var offsetRotation = Quaternion.FromToRotation(_forward, Vector3.forward);
+
+        // 回転補正→ターゲット方向への回転の順に、自身の向きを操作する
+        _self.rotation = lookAtRotation * offsetRotation;
     }
     //private void CreateEnemy()
     //{
