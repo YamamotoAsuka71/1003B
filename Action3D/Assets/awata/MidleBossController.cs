62f1f430e8b40ad5fe11c4cc030d514eb2517b00
awatayuuto <yulovecat9@gmail.com>
2023-12-05 17:09:08 +0900

    Objectの設置/中ボス・ラスポスのスクリプト作成
    神殿の設置・調整/中ポス・ラスボスの攻撃遷移

--- Action3D/Assets/awata/MidleBossController.cs
+++ Action3D/Assets/awata/MidleBossController.cs
@@ -1,55 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MidleBossController : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     GameObject Player;
 
-
+    [SerializeField]
     const float FLY_SPEED = 2.0f;
+    [SerializeField]
+    const float BUTTLE_RENGE = 5.0f;
+    [SerializeField]
+    const float COOL_TIME = 5.0f;
     Animator animator;
+    Material material;
 
     int Hp = 500;
     int stg = 40;
 
     float speed = 0;
+    float timer = 0;
     bool is_attack = false;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        material = gameObject.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        Attack();
     }
 
     void Movement()
     {
+        timer += Time.deltaTime;
+
         Vector3 direction = Player.transform.position - transform.position;
 
         Vector3 normal_dir = direction.normalized;
 
         transform.LookAt(Player.transform.position);
 
-        transform.Translate(transform.forward * FLY_SPEED * Time.deltaTime);
+        if (direction.magnitude > BUTTLE_RENGE)
+        {
+            transform.Translate(transform.forward * FLY_SPEED * Time.deltaTime);
+        }
+        else
+        {
+            if (timer > COOL_TIME)
+            {
+                Attack();
+                timer = 0;
+            }
+        }
+        if (timer > 1.0f)
+        {
+            material.color = Color.white;
+        }
 
-        
-
-        animator.SetFloat("Speed", speed);
+        //animator.SetFloat("Speed", speed);
     }
 
     void Attack()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            animator.SetTrigger("isAttack");
-        }
+        material.color = Color.yellow;
+        //animator.SetTrigger("isAttack");
     }
 }
