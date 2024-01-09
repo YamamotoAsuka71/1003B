cd90777d30810882b55fd818125b6af48846e237
YamamotoAsuka <shira71neko@gmail.com>
2024-01-09 14:35:48 +0900

    カメラのプログラムの更新とそれに伴うオブジェクト・タグの追加

--- Action3D/Assets/Scripts/CameraController.cs
+++ Action3D/Assets/Scripts/CameraController.cs
@@ -4,48 +4,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] GameObject targetObj;
-    Vector3 targetPos;
-
-    float height = 1.0f;
+    [SerializeField] GameObject player;
 
     void Start()
     {
-        targetPos = targetObj.transform.position;
+        
     }
 
     void Update()
     {
-        // targetの移動量分、自分（カメラ）も移動する
-        transform.position += targetObj.transform.position - targetPos;
-        targetPos = targetObj.transform.position;
+        gameObject.transform.position = player.transform.position;
 
-        // マウスの移動量
-        float mouseInputX = Input.GetAxis("Mouse X");
-        float mouseInputY = Input.GetAxis("Mouse Y");
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
 
-        // X方向に一定量移動していれば横回転
-        //0.0000001fは滑らかさ
-        if (Mathf.Abs(mouseInputX) > 0.0000001f)
+        if (Mathf.Abs(mx) > 0.001f)
         {
-            mouseInputX = mouseInputX * 5;
-
-            // 回転軸はワールド座標のY軸
-            transform.RotateAround(targetObj.transform.position, Vector3.up, mouseInputX);
-
+            transform.RotateAround(player.transform.position, Vector3.up, mx);
         }
 
-        // Y方向に一定量移動していれば縦回転
-        if (Mathf.Abs(mouseInputY) > 0.0000001f)
+        if (gameObject.tag != "CameraPos")
         {
-            //高さの制限
-            if ((height - mouseInputY) < -2 || (height - mouseInputY) > 4)
+            if (Mathf.Abs(my) > 0.001f)
             {
-                mouseInputY = 0;
+                transform.RotateAround(player.transform.position, transform.right, my);
             }
-            height -= mouseInputY / 10;
         }
-        // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
-        transform.RotateAround(targetPos, transform.right, mouseInputY);
     }
 }
--- Action3D/Assets/Scripts/PlayerController.cs
+++ Action3D/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -68,57 +69,58 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            Transform cameraTransform = camera.transform;
-            cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, 0.0f, cameraTransform.localPosition.z);
-            Vector3 forward = cameraTransform.forward;
-            forward.x = 0.0f;
-            forward.y = 0.0f;
-            forward = new Vector3(forward.x, forward.y, forward.z);
-            Vector3 right = cameraTransform.right;
-            right.y = 0.0f;
-            right.z = 0.0f;
-            right = new Vector3(right.x, right.y, right.z);
+            //Transform cameraTransform = camera.transform;
+            //cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, 0.0f, cameraTransform.localPosition.z);
+            //Vector3 forward = cameraTransform.forward;
+            //forward.x = 0.0f;
+            //forward.y = 0.0f;
+            //forward = new Vector3(forward.x, forward.y, forward.z);
+            //Vector3 right = cameraTransform.right;
+            //right.y = 0.0f;
+            //right.z = 0.0f;
+            //right = new Vector3(right.x, right.y, right.z);
             collision.SetActive(false);
             animator.SetBool("isAttack", false);
+            Transform cameraTransform = camera.transform;
             //  Aが押されたら
             if (Input.GetKey(KeyCode.A))
             {
                 //  子のオブジェクトから見て右に移動
-                transform.position -= right * Time.deltaTime * speed;
+                transform.position -= camera.transform.right * Time.deltaTime * speed;
                 isMove = true;
             }
             //  Dが押されたら
             if (Input.GetKey(KeyCode.D))
             {
                 //  子のオブジェクトから見て左に移動
-                transform.position += right * Time.deltaTime * speed;
+                transform.position += camera.transform.right * Time.deltaTime * speed;
                 isMove = true;
             }
             //  Wが押されたら
             if (Input.GetKey(KeyCode.W))
             {
                 //  子のオブジェクトから見て奥に移動
-                transform.position += forward * Time.deltaTime * speed;
+                transform.position += camera.transform.forward * Time.deltaTime * speed;
                 isMove = true;
             }
             //  Sが押されたら
             if (Input.GetKey(KeyCode.S))
             {
                 //  子のオブジェクトから見て手前に移動
-                transform.position -= forward * Time.deltaTime * speed;
+                transform.position -= camera.transform.forward * Time.deltaTime * speed;
                 isMove = true;
             }
             //  左のシフトキーが押されたら
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 //  子のオブジェクトから見て下に移動
-                transform.position -= camera.transform.up * Time.deltaTime;
+                transform.position -= transform.up * Time.deltaTime;
             }
             //  スペースキーが押されたら
             if (Input.GetKey(KeyCode.Space))
             {
                 //  子のオブジェクトから見て上に移動
-                transform.position += camera.transform.up * Time.deltaTime;
+                transform.position += transform.up * Time.deltaTime;
             }
 
             if (Input.GetMouseButton(1))
