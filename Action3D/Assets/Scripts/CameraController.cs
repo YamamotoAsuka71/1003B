fb4e4e22b1544d1f8652772506b51b95cd78c0ee
YamamotoAsuka <shira71neko@gmail.com>
2024-01-09 15:29:10 +0900

    カメラの縦回転の方向の手直しと回転角度制限の設定

--- Action3D/Assets/Scripts/CameraController.cs
+++ Action3D/Assets/Scripts/CameraController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] GameObject player;
-
+    [SerializeField] float MinAngleX = -25.0f;
+    [SerializeField] float MaxAngleX = 25.0f;
     void Start()
     {
         
@@ -28,6 +29,21 @@ public class CameraController : MonoBehaviour
             if (Mathf.Abs(my) > 0.001f)
             {
                 transform.RotateAround(player.transform.position, transform.right, my);
+            }
+            float currentXAngle = transform.eulerAngles.x;
+            // Œ»Ý‚ÌŠp“x‚ª180‚æ‚è‘å‚«‚¢ê‡
+            if (currentXAngle > 180)
+            {
+                // ƒfƒtƒHƒ‹ƒg‚Å‚ÍŠp“x‚Í0`360‚È‚Ì‚Å-180`180‚Æ‚È‚é‚æ‚¤‚É•â³
+                currentXAngle = currentXAngle - 360;
+            }
+            if (currentXAngle > MaxAngleX)
+            {
+                transform.eulerAngles = new Vector3(MaxAngleX, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+            if (currentXAngle < MinAngleX)
+            {
+                transform.eulerAngles = new Vector3(360.0f + MinAngleX, transform.eulerAngles.y, transform.eulerAngles.z);
             }
         }
     }
